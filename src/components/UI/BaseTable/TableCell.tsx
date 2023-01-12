8b83c69a6904a2542513fe8fd630321d942fa4b9
Andrey <topt2@ya.ru>
2023-01-12 22:56:00 +0300

    colspan

--- src/components/UI/BaseTable/TableCell.tsx
+++ src/components/UI/BaseTable/TableCell.tsx
@@ -1,9 +1,10 @@
 import React from "react"
 type Props = { 
     children: JSX.Element|JSX.Element[],
+    colspan?: number
   };
 
 
-export const TableCell: React.FC<Props> = ({children}) =>{
-    return(<td>{children}</td>)
+export const TableCell: React.FC<Props> = ({children, colspan = 1}) =>{
+    return(<td colSpan={colspan}>{children}</td>)
 }
--- src/components/UI/ObjectsTable/ObjectsTable.tsx
+++ src/components/UI/ObjectsTable/ObjectsTable.tsx
@@ -19,19 +19,19 @@ const headers = [
   ];
 
 const ObjectsTable = ({rowsData}: Props)=>{
-    const header = <TableRow>{headers.map(h=><TableHeaderCell><div>{h}</div></TableHeaderCell>)}</TableRow>
+    const header = <TableRow>{headers.map(h=><TableHeaderCell {...{key: h}}><div>{h}</div></TableHeaderCell>)}</TableRow>
     const rows = rowsData && rowsData.length > 0 ? 
         rowsData.map(row =>
         <TableRow {...{key: row.DeviceGatewayId, tabIndex: row.DeviceGatewayId}}> 
-            <TableCell><div>{row.Domain}</div></TableCell>
+            <TableCell ><div>{row.Domain}</div></TableCell>
             <TableCell><div>{row.ObjectName}</div></TableCell>
             <TableCell><div>{row.ObjectAddress}</div></TableCell>
             <TableCell><div>{row.ServiceCompany}</div></TableCell>
             <TableCell><div>{row.DeviceGatewayName}</div></TableCell>
             <TableCell><div>{row.AlarmsCount}</div></TableCell>
         </TableRow>):
-        <TableRow><TableCell {...{colspan: headers.length}}><div>{"no data"}</div></TableCell></TableRow>
-        
+        <TableRow><TableCell colspan={headers.length}><div>{"no data"}</div></TableCell></TableRow>
+
     return(
         <Table header={header} rows={rows} />
     )
