e84b4beae20ba454c00aa088522075d424631f77
Andrey <topt2@ya.ru>
2023-02-10 23:14:54 +0300

    стили для таблицы

--- src/UI/Table/Table.tsx
+++ src/UI/Table/Table.tsx
@@ -1,52 +1,52 @@
 import React from "react";
-import "./Table.module.css";
+import styles from "./Table.module.css";
 
-type Props = { 
-  header: React.ReactNode
-  colgroup?: React.ReactNode 
-  rows: React.ReactNode 
-}
+type Props = {
+  header: React.ReactNode;
+  colgroup?: React.ReactNode;
+  rows: React.ReactNode;
+};
 
 const Table = ({ header, colgroup, rows }: Props) => {
   return (
-    <table>
-      <thead>{header}</thead>
-      <colgroup>
-        {colgroup}
-      </colgroup>
-      <tbody>{rows}</tbody>
+    <table className={styles.table}>
+      <thead className={styles.header}>{header}</thead>
+      <colgroup>{colgroup}</colgroup>
+      <tbody className={styles.body}>{rows}</tbody>
     </table>
   );
 };
 
 export default Table;
 
-type TableCellProps = { 
-  children: React.ReactNode,
-  colspan?: number,
-  style?: React.CSSProperties
-  title?: string
+type TableCellProps = {
+  children: React.ReactNode;
+  colspan?: number;
+  style?: React.CSSProperties;
+  title?: string;
 };
 
-
-export const TableCell: React.FC<TableCellProps> = ({children, colspan = 1, style, title}) =>{
-  return(<td style={style} colSpan={colspan} title={title}>{children}</td>)
-}
-
-type TableHeaderCellProps = { 
-  children: React.ReactNode,
+export const TableCell: React.FC<TableCellProps> = ({ children, colspan = 1, style, title }) => {
+  return (
+    <td className={styles.bodycell} style={style} colSpan={colspan} title={title}>
+      {children}
+    </td>
+  );
 };
 
-export const TableHeaderCell: React.FC<TableHeaderCellProps> = ({children}) =>{
-  return(<th>{children}</th>)
-}
-
-type TableRowProps = { 
-  children: React.ReactNode,
-  style?: string
+type TableHeaderCellProps = {
+  children: React.ReactNode;
 };
 
+export const TableHeaderCell: React.FC<TableHeaderCellProps> = ({ children }) => {
+  return <th className={styles.headercell}>{children}</th>;
+};
 
-export const TableRow: React.FC<TableRowProps> = ({children,style}) =>{
-  return(<tr className={style}>{children}</tr>)
-}
+type TableRowProps = {
+  children: React.ReactNode;
+  style?: string;
+};
+
+export const TableRow: React.FC<TableRowProps> = ({ children, style }) => {
+  return <tr className={`${styles.row} ${style}`}>{children}</tr>;
+};
